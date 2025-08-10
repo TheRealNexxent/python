dc99f93fabeb6efe1050c6565e08e7795b195bd1
Nexxent <nexxent@gmail.com>
2025-08-10 18:02:34 +0530

    Added pytest via pip and ready to deploy

--- Chapter 4/setmethods.py
+++
@@ -1,7 +0,0 @@
-y = {1, 1, 1, 1, 2, 3, 4, 5, "Haru"}
-print(y)
-y.add(50)
-print(y, type(y))
-
-y.remove(1)
-print(y)
--- Chapter 5/username.py
+++
@@ -1,5 +0,0 @@
-x = input("Enter your username: ")
-if len(x) < 10:
-    print("Username must be at least 10 characters long.")
-else:
-    print("Username Accepted.")
